20173f814a13216ea1a9e6f65176e38566fed77e
sergio <serhiypetrovskyy@gmail.com>
2018-12-14 15:49:31 +0200

    Chips and Hand classes are added

--- black_jack.py
+++ black_jack.py
@@ -2,12 +2,12 @@
 #shuffle card deck
 import random 
 
-#Creating a list, tuple, dictionary to store
+#Creating a tuples, dictionary to store
 #card suits, ranks, and values
 
 suits = ("Hearts", "Diamonds", "Spades", "Clubs")
 ranks = ("Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
-values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": (10, 1)}
+values = {"Two": 2, "Three": 3, "Four": 4, "Five": 5, "Six": 6, "Seven": 7, "Eight": 8, "Nine": 9, "Ten": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": 11}
 
 #Declaring boolean value to control while loop and 
 #flow of the game
@@ -51,9 +51,59 @@ class Deck:
     	return single_card
 
 
-test_deck = Deck()
-test_deck.shuffle()
-print(test_deck)
+class Hand:
+	def __init__(self):
+		self.cards = []
+		self.value = 0
+		self.aces = 0
+
+	def add_card(self, card):
+		self.cards.append(card)
+		self.value = self.value + values[card.rank]
+		if card.rank == "Ace":
+			self.aces += 1
+
+	def adjust_for_ace():
+		while self.value > 21 and self.aces:
+			self.value = self.value - 10
+			self.aces -= 1
+
+class Chips:
+	def __init__(self):
+		self.total = 100
+		self.bet = 0
+
+	def win_bet(self):
+		self.total = self.total + self.bet
+
+	def lose_bet(self):
+		self.total = self.total - self.bet
+
+
+def take_bet(chips):
+    while True:
+    	try:
+		    chips.bet = int(input("How many chips would you like to bet? "))
+
+		except: 
+			print("Your bet must be a number!S")
+			
+		else:
+			if chips.bet > chips.total:
+				print("Sorry, your bet can't exceed", chips.total)
+			else:
+			    print("Thanks. Bet accepted.")
+			    break
+
+
+my_deck = Deck()
+my_deck.shuffle()
+player_1 = Hand()
+player_1.add_card(my_deck.deal())
+player_1.add_card(my_deck.deal())
+print(player_1.value)
+for card in player_1.cards:
+	print(card)
 
 
 
